c6937b83986b174fad2870473273dc9a51c6a9ef
arbahi jawad <jawad.arbahi01@gmail.com>
2023-09-17 16:55:26 +0100

    structure of the task manager project

---
+++ src/main/java/com/example/task_management_system/TaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementSystemApplication.class, args);
+    }
+
+}
